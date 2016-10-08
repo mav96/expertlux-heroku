95cf90b9a2f04149f46f9255e9dbddadad6ebf9c
Васильев Петр Сергеевич <vasilev@proitr.ru>
2016-10-08 12:27:19 +0500

    mailgun server return invalid responce(text) fix

--- src/controllers/mail-controller.ts
+++ src/controllers/mail-controller.ts
@@ -1,4 +1,4 @@
-import {Router, Request, Response, NextFunction} from "express";
+import { Router, Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
 import MailService from "../services/mailgun-service";
 
@@ -11,13 +11,22 @@ ctrl.post("/", bodyParser.json(), async (req: Request, res: Response, next: Next
     let subject = "Кричалка. " + request.type;
     let text = `${request.name} ${request.phone}`;
     let service = new MailService();
-    service.send(from, to, subject, text).then((responce) => {
-        return responce.json();
-    }).then((json) => {
-        res.json(json);
-    }).catch((err) => {
+
+    try {
+        service.send(from, to, subject, text)
+            .then((responce) => responce.text())
+            .then((text) => {
+                // mailgun server return invalid responce(text)
+                try {
+                    let json = JSON.parse(text);
+                    res.json(json);
+                } catch (err) {
+                    next(err);
+                }
+            });
+    } catch (err) {
         next(err);
-    });
+    }
 });
 
 class CrierRequest {
